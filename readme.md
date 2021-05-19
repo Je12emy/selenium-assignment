83a738a15569f8e1525bfa7db8efd74343064739
Jeremy <jeremyzelaya@hotmail.es>
2021-05-19 14:10:40 -0600

    Created repo

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Instructions
+
+- Write some high level keywords. ie `explicit`, `implic`, `assert`
+- Write atleast 20 test scripts.
+- Add [Allure](https://docs.qameta.io/allure/) or other for reporting.
+- Run parallel/group tests.
+- Run only with `Test.xml` (collective test cases).
+- Run with Maven configuration.
---
+++ src/main/java/com/swaglabs/swaglabs/SwaglabsApplication.java
@@ -0,0 +1,13 @@
+package com.swaglabs.swaglabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaglabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaglabsApplication.class, args);
+	}
+
+}
