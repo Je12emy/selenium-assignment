2c0cc27bf50758353ad155bd9fe7b2a99cc62c5c
Jeremy <jeremyzelaya@hotmail.es>
2021-05-20 15:06:38 -0600

    Removed spring dependencies

--- src/main/java/com/swaglabs/swaglabs/SwaglabsApplication.java
+++ src/main/java/com/swaglabs/swaglabs/SwaglabsApplication.java
@@ -1,13 +1,8 @@
 package com.swaglabs.swaglabs;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SwaglabsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SwaglabsApplication.class, args);
 	}
 
 }
